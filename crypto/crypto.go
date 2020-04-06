710c91cf5f682a8b9197675320b8125ea0cb0ac8
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-04-06 15:09:06 -0700

    Very smol basic knapsack cryptosystem implemented

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,6 +1,63 @@
 package crypto
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+// Knapsack contains the private data used to generate the public key and decrypt messages
+type Knapsack struct {
+	PublicKey  []int64
+	privateKey []int64
+	m          int64 // modulus
+	w          int64 // random mutating constant
+	wi         int64 // inverse of w
+}
+
+// NewKnapsack auto generates private knapsack params
+func NewKnapsack(keyLength int) (*Knapsack, error) {
+	if keyLength > 56 { // since we aren't using big.Int yet, we're a little limited here
+		return nil, errors.New("key length must be <= 56")
+	}
+	// private sequence should begin at around 2^n (n is the sequence length)
+	// and should end around 2^2n
+	min := math.Pow(2, float64(keyLength))
+	// max := math.Pow(2, float64(2*keyLength))
+
+	// start by generating a random superincreasing sequence
+	// TODO make this random and not stupid
+	privateKey := []int64{int64(min + 1)}
+	sumSoFar := int64(min + 1)
+	for len(privateKey) < keyLength {
+		privateKey = append(privateKey, sumSoFar+1)
+		sumSoFar += sumSoFar + 1
+	}
+
+	// the modulus should be greater than sum(privateKey)
+	// TODO make this random and not stupid
+	m := sumSoFar + 1
+	w := randomCoprime(m)
+	wi, err := inverse(w, m)
+	if err != nil {
+		return nil, err
+	}
+
+	// calculate public key (for now, doing now index mangling)
+	publicKey := make([]int64, len(privateKey))
+	for idx, n := range privateKey {
+		publicKey[idx] = (n * w) % m
+	}
+
+	return &Knapsack{
+		PublicKey:  publicKey,
+		privateKey: privateKey,
+		m:          m,
+		w:          w,
+		wi:         wi,
+	}, nil
+}
 
 // Encrypt uses `message` [m0, ..., mn] as an index map on the public
 // key [p0, ..., pn] to compute ct = sum(m*p). The PK and output are int64
@@ -15,6 +72,16 @@ func Encrypt(publicKey []int64, message []byte) (int64, error) {
 		ct += publicKey[idx] * int64(bit)
 	}
 	return ct, nil
+}
+
+// Decrypt uses the private information to solve the knapsack problem and returns
+// the message as a slice of bits.
+func (k *Knapsack) Decrypt(ct int64) []byte {
+	// undo the mutation of `w`
+	c := (ct * k.wi) % k.m
+	// solve the knapsack problem with weights=privateKey, target=c
+	msg := solveKnapsack(k.privateKey, c)
+	return msg
 }
 
 // StringToBits returns a slice of [x0, x1, ..] where xi is 0 or 1.
@@ -33,4 +100,93 @@ func StringToBits(s string) []byte {
 		}
 	}
 	return bitsOfStr
+}
+
+// returns the mask (e.g. [0, 1, 1, 0]) of the weights to choose to reach target
+// this function assumes:
+//   a) the private key is a superincreasing sequence
+//   b) a solution exists (at least for now...may add failure cases later)
+func solveKnapsack(weights []int64, s int64) []byte {
+	var solve func([]int64, int64, int64)
+	solution := make([]byte, len(weights))
+	solutionIdx := len(weights) - 1
+	solve = func(remainingWeights []int64, sumOfRemainingWeights int64, target int64) {
+		if len(remainingWeights) == 0 || target == 0 {
+			return
+		}
+		last := remainingWeights[len(remainingWeights)-1]
+		weightsWithoutLast := remainingWeights[:len(remainingWeights)-1]
+		sumWithoutLast := sumOfRemainingWeights - last
+
+		if target > sumWithoutLast {
+			solution[solutionIdx] = 1
+			solutionIdx--
+			solve(weightsWithoutLast, sumWithoutLast, target-last)
+		} else {
+			solution[solutionIdx] = 0
+			solutionIdx--
+			solve(weightsWithoutLast, sumWithoutLast, target)
+		}
+	}
+	solve(weights, sum(weights), s)
+	return solution
+}
+
+// reduces the array with summation fn
+func sum(arr []int64) int64 {
+	var sum int64 = 0
+	for _, n := range arr {
+		sum += n
+	}
+	return sum
+}
+
+// compute the multiplicate inverse of a mod n
+func inverse(a, n int64) (int64, error) {
+	var t, newt int64
+	var r, newr int64
+
+	t, newt = 0, 1
+	r, newr = n, a
+
+	for newr != 0 {
+		var quotient int64 = r / newr
+		t, newt = newt, t-quotient*newt
+		r, newr = newr, r-quotient*newr
+	}
+	if r > 1 {
+		return 0, fmt.Errorf("%d is not invertible mod %d", a, n)
+	}
+	if t < 0 {
+		t = t + n
+	}
+	return t, nil
+}
+
+// compute a mod n without the float64 bs
+func mod(a, n int64) int64 {
+	if a < 0 {
+		a = -a
+	}
+	if a < n {
+		return a
+	}
+	q := a / n
+	return a - (n * q)
+}
+
+func gcd(a, b int64) int64 {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+// get a random number m such that gcd(m,n)=1
+func randomCoprime(n int64) int64 {
+	var r int64
+	for r < 2 || gcd(r, n) != 1 {
+		r = rand.Int63n(n)
+	}
+	return r
 }
--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -5,13 +5,45 @@ import (
 	"testing"
 )
 
+func TestDecrypt(t *testing.T) {
+	k, err := NewKnapsack(8)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msg := StringToBits("h")
+	ct, err := Encrypt(k.PublicKey, msg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if d := k.Decrypt(ct); !bytes.Equal(d, msg) {
+		t.Errorf("wanted %v, got %v", msg, d)
+	}
+}
+
+func isSuperincreasingSequence(arr []int64) bool {
+	if len(arr) < 2 {
+		return true
+	}
+	sum := arr[0]
+
+	for i := 1; i < len(arr); i++ {
+		if arr[i] <= sum {
+			return false
+		}
+		sum += arr[i]
+	}
+	return true
+}
+
 func TestEncrypt(t *testing.T) {
 	msg := []byte{1, 1, 0, 0, 1, 1, 0}
 	pk := []int64{1, 2, 3, 4, 5, 6, 7}
 	var expected int64 = 14 // 1 + 2 + 5 + 6 = 14
 	actual, err := Encrypt(pk, msg)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if actual != expected {
 		t.Errorf("wanted %v, got %v", expected, actual)
@@ -23,5 +55,45 @@ func TestStringToBits(t *testing.T) {
 	actual := StringToBits("hello")
 	if !bytes.Equal(actual, expected) {
 		t.Errorf("wanted %v, got %v", expected, actual)
+	}
+}
+
+func TestInverse(t *testing.T) {
+	type testCase struct {
+		a        int64
+		n        int64
+		expected int64
+	}
+	testCases := []testCase{
+		{a: 588, n: 881, expected: 442},
+		{a: 3, n: 2000, expected: 667},
+	}
+
+	for idx, tc := range testCases {
+		actual, err := inverse(tc.a, tc.n)
+		if err != nil {
+			t.Fatalf("encountered error with inputs %d, %d: %v", tc.a, tc.n, err)
+		}
+		if actual != tc.expected {
+			t.Errorf("for test case #%d: wanted %v, got %v", idx, tc.expected, actual)
+		}
+	}
+}
+
+func TestSolveKnapsack(t *testing.T) {
+	type testCase struct {
+		weights  []int64
+		s        int64
+		expected []byte
+	}
+	testCases := []testCase{
+		{weights: []int64{5, 10, 17, 33, 70}, s: 32, expected: []byte{1, 1, 1, 0, 0}},
+	}
+
+	for idx, tc := range testCases {
+		actual := solveKnapsack(tc.weights, tc.s)
+		if !bytes.Equal(actual, tc.expected) {
+			t.Errorf("for test case #%d: wanted %v, got %v", idx, tc.expected, actual)
+		}
 	}
 }
